b38c31fbd291d9334643d763e52202934a5f9d7e
Rakshith Kumar <rakshithkumab01718@gmail.com>
2026-03-27 19:16:18 +0530

    feat: server and database connected

--- server/server.js
+++ server/server.js
@@ -1,27 +1,13 @@
-// server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require("./src/config/db");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+connectDB();
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('Backend server is running 🚀');
-});
-
-// Example API endpoint
-app.post('/api/data', (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `Hello, ${name}! Your data was received.` });
-});
-
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  console.log(`Server running on port ${PORT} 🔥`);
+  
+})
+  
