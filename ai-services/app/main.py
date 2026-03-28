931c5775ffe8c1f7f7e8db6f06985a21258430e4
Rakshith Kumar <rakshithkumab01718@gmail.com>
2026-03-28 19:33:13 +0530

    feat: main.py changed

--- ai-services/app/main.py
+++ ai-services/app/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
 from app.routes.agent_routes import router
-from app.rag.init_data import load_initial_data
-
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
-    try:
-        print("🚀 Loading RAG initial data...")
-        load_initial_data()
-        print("✅ Data loaded successfully")
-    except Exception as e:
-        print("❌ Error loading data:", str(e))
+    print("🚀 Startup running (no data load)")
 
 app.include_router(router, prefix="/agent")
